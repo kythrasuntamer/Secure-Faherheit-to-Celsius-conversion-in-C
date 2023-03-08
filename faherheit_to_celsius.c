b81b7973a99669fb366a5fe4daae4e1233954335
Paul Michael Tidwell <121031213+kythrasuntamer@users.noreply.github.com>
2023-03-08 18:14:32 +0000

    Create faherheit_to_celsius.c

--- faherheit_to_celsius.c
+++ faherheit_to_celsius.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 #include "float-input.h"
 
 int main() {
@@ -7,9 +10,15 @@ int main() {
     puts("Enter temperature in Fahrenheit: ");
     getFloatFromStdin(&fahr);
 
+    while (isnan(fahr) || isinf(fahr)) {
+        fprintf(stderr, "[ERROR]: Invalid input: please enter a valid temperature.\n");
+        puts("Enter temperature in Fahrenheit: ");
+        getFloatFromStdin(&fahr);
+    }
+
     celsius = (fahr- 32) * 5 / 9;
 
-    printf("%3.0f %6.1f\n", fahr, celsius);
+    printf("%3.0f %6.10f\n", fahr, celsius);
 
     return 0;
 }
